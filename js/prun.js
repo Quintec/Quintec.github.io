67e15e1eeceb70b7adc0c4ff35493e1b949c3d47
Quintec <quintek55555@gmail.com>
2021-08-10 23:23:15 -0400

    particle speed

--- js/prun.js
+++ js/prun.js
@@ -56,7 +56,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
